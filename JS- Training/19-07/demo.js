90a9ba829fd949a92632fda670603dd4cdff1a98
PranjulChaturvedi-cpu <ukautomobile0@gmail.com>
2024-07-23 13:11:37 +0530

    Create demo.js

---
+++ JS- Training/19-07/demo.js
@@ -0,0 +1,30 @@
+
+
+// var a=10; var b=20
+// console.log(a++);
+// console.log(a);
+// console.log(b--);
+// console.log(b);
+// console.log(++a);
+// console.log(--b);
+
+// var a=10
+// console.log(a++,++a,--a,a--,--a,a++,a);
+
+// var a=5
+// console.log(a++,a+=5,++a,a>2,a==="5",a++,++a,a*2,a);
+
+// var a=15
+// console.log(++a,a--,a+=10,a=50,++a,a--,a*=2,++a);
+
+console.log(7>2 && 12>10 && 15==15);
+console.log(7>2 && 12>10 && 15==25);
+console.log(7>2 && 12<10 && 15==25);
+console.log(7<2 && 12<10 && 15==25);
+
+console.log(7>2 || 12>10 || 15==15);
+console.log(7>2 || 12>10 || 15==35);
+console.log(7>2 || 12<10 || 15==35);
+console.log(7<2 || 12<10 || 15==35);
+console.log(!(7<2 || 12<10 || 15==35));
+console.log(!(7>2 || 12<10 || 15==35));
